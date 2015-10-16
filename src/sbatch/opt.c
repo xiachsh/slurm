aece872110a61f9133243451e3a7baf0e6c8a248
Bill.Brophy <Bill.Brophy@atos.net>
2015-10-16 07:30:23 -0700

    set of changes for sbatch support

--- src/sbatch/opt.c
+++ src/sbatch/opt.c
@@ -931,6 +931,9 @@ char *process_options_first_pass(int argc, char **argv)
 		      " --wrap option.");
 		exit(error_exit);
 	}
+
+	if (packjob && !packleader) return NULL;
+
 	if (argc > optind) {
 		int i;
 		char **leftover;
@@ -2559,7 +2562,7 @@ static void _parse_pbs_resource_list(char *rl)
  */
 static bool _opt_verify(void)
 {
-bool verified = true;
+	bool verified = true;
 	char *dist = NULL, *lllp_dist = NULL;
 	uint32_t cluster_flags = slurmdb_setup_cluster_flags();
 
