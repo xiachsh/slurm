4235fbbe5072e234aa79aa23537944cfa483732c
Rod.Schultz <Rod.Schultz@atos.net>
2016-02-19 11:03:03 -0700

    Fix problems in runnable_ever test. Clean up debug messages. Added function to create a nodelist of all nodes in a job pack.
     Some corrections for preemption
    
     fixes & updates for job pack.
     Fix salloc missing closing brace
     Fix sbatch processing of #ENVs defined in script file

--- src/common/env.h
+++ src/common/env.h
@@ -323,4 +323,9 @@ char *uint32_compressed_to_str(uint32_t array_len,
 			       const uint16_t *array,
 			       const uint32_t *array_reps);
 
+/*
+ * TODO
+ */
+extern void env_array_for_packenv(char ***env_array, char *name, char *value);
+
 #endif
