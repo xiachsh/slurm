dfc867aac83ac590d4af7099f6e038ccb7b20fdc
Rod.Schultz <Rod.Schultz@atos.net>
2016-03-07 09:46:47 -0700

    update to old env.c file to run correctly Fix dependency problem that makes test1.42 fail. Removed some info messages. Fixed seg fault when job has already completed. Fixed compiler warning

--- src/slurmctld/job_scheduler.c
+++ src/slurmctld/job_scheduler.c
@@ -2359,10 +2359,6 @@ extern void print_job_dependency(struct job_record *job_ptr)
 			dep_str = "packleader";
 		else if (dep_ptr->depend_type == SLURM_DEPEND_EXPAND)
 			dep_str = "expand";
-		else if (dep_ptr->depend_type == SLURM_DEPEND_PACK)
-			dep_str = "pack";
-		else if (dep_ptr->depend_type == SLURM_DEPEND_PACKLEADER)
-			dep_str = "packleader";
 		else
 			dep_str = "unknown";
 
@@ -2420,10 +2416,6 @@ static void _depend_list2str(struct job_record *job_ptr, bool set_or_flag)
 			dep_str = "packleader";
 		else if (dep_ptr->depend_type == SLURM_DEPEND_EXPAND)
 			dep_str = "expand";
-		else if (dep_ptr->depend_type == SLURM_DEPEND_PACK)
-			dep_str = "pack";
-		else if (dep_ptr->depend_type == SLURM_DEPEND_PACKLEADER)
-			dep_str = "packleader";
 		else
 			dep_str = "unknown";
 
@@ -2656,10 +2648,6 @@ extern int test_job_dependency(struct job_record *job_ptr)
 				job_ptr->details->whole_node =
 					djob_ptr->details->whole_node;
 			}
-		} else if (dep_ptr->depend_type == SLURM_DEPEND_PACK) {
-			depends = true;
-		} else if (dep_ptr->depend_type == SLURM_DEPEND_PACKLEADER) {
-			depends = true;
 		} else
 			failure = true;
 		if (clear_dep) {
